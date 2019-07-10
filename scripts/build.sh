01479d3b7066e62a8ec85925c0695807229a7f47
Shingo Sato <shinsugar@gmail.com>
2019-07-10 12:50:22 +0900

    Use shellcheck

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,17 +14,17 @@ readonly cname=slides.sugarshin.net
 mkdir -p ${dist} ${pdf_dist}
 
 for p in ${publishes[@]}; do
-  target=$(basename $(dirname "$p"))
-  mkdir -p ${dist}/${target}
+  target=$(basename "$(dirname "$p")")
+  mkdir -p "${dist}/${target}"
   file=${src}/${target}/index.md
   echo "Build $file ..."
-  ${marp} $file --html -o ${dist}/${target}/index.html
-  ${marp} $file --html --pdf --allow-local-files -o ${pdf_dist}/${target}.pdf
-  ${marp} $file --image png -o ${dist}/${target}/index.png
+  ${marp} "$file" --html -o "${dist}/${target}/index.html"
+  ${marp} "$file" --html --pdf --allow-local-files -o "${pdf_dist}/${target}.pdf"
+  ${marp} "$file" --image png -o "${dist}/${target}/index.png"
 
   echo "Copy ${src}/${target}/${images_dir} ..."
-  mkdir -p ${dist}/${target}/${images_dir}
-  find ${src}/${target}/${images_dir} -type f -iname "*.png" -exec cp {} ${dist}/${target}/${images_dir}/ \;
+  mkdir -p "${dist}/${target}/${images_dir}"
+  find "${src}/${target}/${images_dir}" -type f -iname "*.png" -exec cp {} "${dist}/${target}/${images_dir}/" \;
   # TODO: other image ext
 done
 
